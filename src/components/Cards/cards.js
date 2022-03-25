60ceb587f394627ad8794c27cfac110c1b2197b5
Tomas Fortunato <tomasfortunato@hotmail.com>
2022-03-24 23:47:57 -0300

    Clase 4

--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -1,15 +1,23 @@
-import Button from '@mui/material/Button';
+import React,{ useState} from 'react'
 
-function card({title, dias, precio}) {
+
+
+export default function Card({data}) {
+    const {title, dias, precio}= data
+    const [ count, setCount ] = useState(0)
+
+    console.log("estado contador: ", count)
+
+    const addStock = () => {
+        setCount(count + 1)
+    }
 
     return(
         <div className="card-item">
             <h2>{title}</h2>
             <p>Entrenamientos Semanales : <br></br>{dias}</p>
             <p>Precio : $ {precio}</p>
-            <Button variant="contained"> Subscribirme </Button>
+            <button onClick={addStock}> Subscribirme </button>
         </div>
     )
 }
-
-export default card
--- src/components/ListProducts/listProducts.js
+++ src/components/ListProducts/listProducts.js
@@ -1,12 +1,16 @@
 import Card from '../Cards/cards'
 
-const listProducts = () =>{
+const listProducts = ({children}) =>{
+    let dataProduct = {
+        title:'Cuota Simple',
+        dias:'1 a 3',
+        precio: 4500
+    }
+
     return(
         <div className="container-cards">
-            <Card title={'Cuota Simple'} dias={'1 a 3'} precio={4500}/>
-            <Card title={'Cuota Completa'} dias={'Libre'} precio={5500}/>
-            <Card title={'Clase Unica'} dias={'1'} precio={1500}/>
-            <Card title={'Clase Individual'} dias={'1'} precio={2500}/>
+            <h2> {children}</h2>
+            <Card data={dataProduct}/>
         </div>
     )
 }
